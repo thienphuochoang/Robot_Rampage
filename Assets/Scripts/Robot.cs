c179ce338f465cb9cccc7788ab37cdef5502df2e
PhuocHoang <htphuoc1995@gmail.com>
2023-11-07 00:38:05 -0500

    Chapter 10 - Add robot script and edit prefabs

---
+++ Assets/Scripts/Robot.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour
+{
+    [SerializeField]
+    private string robotType;
+    public int health;
+    public int range;
+    public float fireRate;
+    public Transform missileFireSpot;
+    UnityEngine.AI.NavMeshAgent agent;
+    private Transform player;
+    private float timeLastFired;
+    private bool isDead;
+    void Start()
+    {
+        // 1
+        isDead = false;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // 2
+        if (isDead)
+        {
+            return;
+        }
+        // 3
+        transform.LookAt(player);
+        // 4
+        agent.SetDestination(player.position);
+        // 5
+        if (Vector3.Distance(transform.position, player.position) < range
+            && Time.time - timeLastFired > fireRate)
+        {
+            // 6
+            timeLastFired = Time.time;
+            fire();
+        }
+    }
+    private void fire()
+    {
+        Debug.Log("Fire");
+    }
+}
