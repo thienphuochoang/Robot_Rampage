c37d308fa291cfa8198caff613beb38ba9b88b97
PhuocHoang <htphuoc1995@gmail.com>
2023-11-07 00:44:04 -0500

    Chapter 10 - Animating robots

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -14,6 +14,7 @@ public class Robot : MonoBehaviour
     private Transform player;
     private float timeLastFired;
     private bool isDead;
+    public Animator robot;
     void Start()
     {
         // 1
@@ -44,6 +45,6 @@ public class Robot : MonoBehaviour
     }
     private void fire()
     {
-        Debug.Log("Fire");
+        robot.Play("Fire");
     }
 }
